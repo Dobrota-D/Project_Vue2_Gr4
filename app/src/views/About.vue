0a8b891f668736d94080d7d5836c75d919330b5f
Léo <leo.gregori95@gmail.com>
2021-10-13 16:45:14 +0200

    router link init

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque perferendis obcaecati voluptatem alias porro explicabo quibusdam sequi commodi dignissimos ipsa? Illo id culpa reiciendis dignissimos alias, tempora necessitatibus aut perspiciatis.</h1>
   </div>
 </template>
---
+++ app/src/views/Admin.vue
@@ -0,0 +1,6 @@
+// Template de la page admin
+<template>
+    <div class="admin">
+        <h1> Admin page </h1>
+    </div>
+</template>
