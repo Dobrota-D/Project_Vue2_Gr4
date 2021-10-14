3c1741314057a4834aa9dbb644f08391d3e80952
Ugo <ugo.prenat@gmail.com>
2021-10-14 11:09:15 +0200

    CryptoList and CryptoCard components

---
+++ app/src/components/CryptoCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="crypto-card">
+        <div class="crypto-data">
+            <img :src="crypto.icon" :alt="crypto.abbreviation + '-icon'">
+            <div class="crypto-name">
+                <p>{{ crypto.name }}</p>
+                <p>{{ crypto.abbreviation }}</p>
+            </div>
+        </div>
+        <div class="crypto-price">
+            <p>{{ cryptoPrice.substring(0, 8) }}€</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        crypto: Object
+    },
+    methods: {
+        getCryptoData(cryptoAbb) {
+            fetch(`https://api.coinbase.com/v2/prices/${cryptoAbb}-EUR/spot`)
+            .then(res => res.json())
+            .then(result => this.cryptoPrice = result.data.amount )
+        }
+    },
+    mounted() {
+        this.getCryptoData(this.crypto.abbreviation)
+    },
+    data() {
+        return {
+            cryptoPrice: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+    p {
+        margin: 0;
+    }
+    .crypto-card {
+        display: flex;
+        justify-content: space-between;
+        background-color: white;
+        border-radius: 7px;
+        padding: 20px;
+        margin: 15px 0;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, .1);
+    }
+    .crypto-data {
+        display: flex;
+        align-items: center;
+    }
+    .crypto-data img {
+        width: 40px;
+        height: 40px;
+        margin-right: 10px;
+    }
+    .crypto-name {
+        display: flex;
+        flex-direction: column;
+    }
+    .crypto-name p {
+        display: flex;
+    }
+    .crypto-price {
+        display: flex;
+        align-items: center;
+    }
+</style>
---
+++ app/src/components/CryptoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="crypto-list">
+        <crypto-card v-for="crypto in cryptoList" :key="crypto.name" :crypto="crypto" />
+    </div>
+</template>
+
+<script>
+    import cryptoListFile from '../data/crypto.data.json'
+    import CryptoCard from './CryptoCard.vue';
+
+export default {
+    props: {
+        nom: String,
+        x: Array
+    },
+    components: { CryptoCard },
+    data() {
+        return {
+            cryptoList: cryptoListFile,
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .crypto-list {
+        width: 45%;
+    }
+</style>
