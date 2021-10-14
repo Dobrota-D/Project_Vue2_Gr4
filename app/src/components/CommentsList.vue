daca267947be61c101bc2b8214d706bab283da84
Dobrota <dobrotakevin@yahoo.fr>
2021-10-14 15:01:21 +0200

    comments componenets

--- app/src/components/CommentsList.vue
+++ app/src/components/CommentsList.vue
@@ -1,11 +1,24 @@
 <template>
-
+    <div class="comments-list">
+        <comments-card v-for="(comment, index) in commentsList" :key="comment.index" :index="index" :comment="comment" />
+    </div>
 </template>
 
 
 <script>
-import comments_List from '../data/comments.data.json'
-
-
-
+import commentsListFile from '../data/comments.data.json'
+import CommentsCard from './CommentsCard.vue'
+export default {
+    props: {
+        index: Number,
+    },
+    components: {
+        CommentsCard,
+    },  
+    data() {
+        return {
+            commentsList: commentsListFile
+        }
+    },
+}
 </script>
