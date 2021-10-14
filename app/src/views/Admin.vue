1f642caa296d9a744d2302ed5111290a01594e92
Dobrota <dobrotakevin@yahoo.fr>
2021-10-14 13:22:33 +0200

    fix admin cryptocard

--- app/src/views/Admin.vue
+++ app/src/views/Admin.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="admin">
         <h1> Admin page </h1>
-        
+        <crypto-list/>
     </div>
 </template>
 
 <script>
-
+import CryptoList from '@/components/CryptoList.vue'
+    
+    export default {
+        components: { CryptoList },
+    }
 </script>
