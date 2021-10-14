a09d766952a75a0c32507bd49ff8ca92498e9b3a
Léo <leo.gregori95@gmail.com>
2021-10-14 13:51:29 +0200

    404 Not Found created and Crypto's single page

---
+++ app/src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="notFound">
+    <h1>(404) Page NotFound</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
