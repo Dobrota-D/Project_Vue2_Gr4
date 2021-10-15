621eead0abbc85c179a67491e3b029ec9003a8ed
Léo <leo.gregori95@gmail.com>
2021-10-15 14:31:39 +0200

    add ЖД

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="about">
-    <h1> Projet en Vue.Js </h1>
+    <h1 class="title"> Projet en Vue.Js </h1>
     <h2> Un site qui présente une liste de crypto parmi une liste prédéfini ainsi que des commentaires. </h2>
     <h3> Produit par Ugo PRENAT, Kevin DOBROTA, Léo GREGORI </h3>
     <h5> 😭😭 RIP LE KARMA Coin (Il est pas sur l'API)😭😭 </h5>
+    <div class="cheminDeFer">
+      <h1> 🛤 </h1>
+      <h1> 🛤 </h1>
+      <h1> 🛤 </h1>
+      <h1> 🛤 </h1>
+    </div>
+    
   </div>
 </template>
 
 <style scoped>
-  h1{
-    margin: 10% 0 5% 0;
+  .title{
+    margin: 5% 0;
   }
   h3{
     margin: 3% 0 0 0 ;
     opacity: 0.7;
+  }
+  .cheminDeFer{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  h1{
+    margin: 0;
   }
 </style>
